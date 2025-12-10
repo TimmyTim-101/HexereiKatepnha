55140896c12d0e6c1b5932f333d2bd3eed38dd60
MomoseNaoichi <joe.10@qq.com>
2025-12-10 22:29:12 +0100

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexereiKatepnha.Models
+{
+    internal class User
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HexereiKatepnha.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _userName = string.Empty;
+
+        [ObservableProperty]
+        private string _greetingMessage = "等待输入...";
+
+        [RelayCommand]
+        private void SayHello()
+        {
+            if (string.IsNullOrWhiteSpace(UserName))
+            {
+                GreetingMessage = "请输入名字！";
+            }
+            else
+            {
+                // 模拟业务逻辑
+                GreetingMessage = $"你好, {UserName}! 欢迎使用 VS2026 风格的应用。";
+            }
+        }
+    }
+}
